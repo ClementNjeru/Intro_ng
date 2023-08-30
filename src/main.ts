32cc4417414b4a0f2c988b267716a0f0120556db
ClementNjeru <njeruclement36@gmail.com>
2023-08-30 10:22:12 +0300

    routes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { provideRouter } from '@angular/router';
 import { AppModule } from './app/app.module';
+import routeConfig from './app/routes';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  }
+).catch(err => console.error(err));
+  
